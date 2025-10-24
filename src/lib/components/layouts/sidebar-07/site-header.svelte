ed5b49424c0f9a3458b17ca0aeaf0a79443b432a
Marlon Villarama <marlonsvillarama@gmail.com>
2025-10-24 13:31:42 +1300

    20251024 - Quotations site header

--- src/lib/components/layouts/sidebar-07/site-header.svelte
+++ src/lib/components/layouts/sidebar-07/site-header.svelte
@@ -3,7 +3,7 @@
 	import { Separator } from "$lib/components/ui/separator/index.js";
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
 
-	let { title = 'Page Title' } = $props();
+	let { children, title = 'Page Title' } = $props();
 </script>
 
 <header
@@ -13,17 +13,8 @@
 		<Sidebar.Trigger class="-ml-1" />
 		<Separator orientation="vertical" class="mx-2 data-[orientation=vertical]:h-4" />
 		<h1 class="text-base font-medium">{title}</h1>
-		<!-- <div class="ml-auto flex items-center gap-2">
-			<Button
-				href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
-				variant="ghost"
-				size="sm"
-				class="dark:text-foreground hidden sm:flex"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				GitHub
-			</Button>
-		</div> -->
+		<div class="ml-auto flex items-center gap-2">
+			{@render children?.()}
+		</div>
 	</div>
 </header>
--- src/routes/app/quotations/+page.svelte
+++ src/routes/app/quotations/+page.svelte
@@ -1,12 +1,24 @@
 <script>
 	import data from "./data.js";
+	import { Button } from "$lib/components/ui/button/index.js";
 	import SiteHeader from "$lib/components/layouts/sidebar-07/site-header.svelte";
 	// import SectionCards from "$lib/components/layouts/dashboard-01/section-cards.svelte";
 	// import ChartAreaInteractive from "$lib/components/layouts/dashboard-01/chart-area-interactive.svelte";
 	import DataTable from "$lib/components/data-table/quotations/data-table.svelte";
 </script>
 
-<SiteHeader title='Quotations' />
+<SiteHeader title='Quotations'>
+	<Button
+		href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
+		variant="ghost"
+		size="sm"
+		class="dark:text-foreground hidden sm:flex"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		GitHub
+	</Button>
+</SiteHeader>
 <div class="flex flex-1 flex-col">
 	<div class="@container/main flex flex-1 flex-col gap-2">
 		<div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
